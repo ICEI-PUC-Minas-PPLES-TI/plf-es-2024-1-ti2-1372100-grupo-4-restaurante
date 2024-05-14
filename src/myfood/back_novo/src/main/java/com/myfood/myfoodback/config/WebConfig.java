4060376c45836a4aec0b2848eb9bc33a3bea822f
Breno Brandão <brenoca35@gmail.com>
2024-05-14 10:48:45 -0300

    cors

--- src/myfood/back_novo/src/main/java/com/myfood/myfoodback/config/WebConfig.java
+++ src/myfood/back_novo/src/main/java/com/myfood/myfoodback/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://plf-es-2024-1-ti2-1372100-grupo-4-restaurante-deploy-518euk7ox.vercel.app/")
+                .allowedOrigins("https://plf-es-2024-1-ti2-1372100-grupo-4-restaurante-deploy-518euk7ox.vercel.app/","http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
